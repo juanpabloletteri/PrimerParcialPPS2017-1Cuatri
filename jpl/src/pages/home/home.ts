5dbe5dc058099fd5a4cf82885359cec6caa19901
juanpabloletteri <pabloletteri@gmail.com>
2017-04-14 22:13:31 -0300

    inputs y boton para login
    se agregaron las entradas para datos y el boton login, tambien la
    funcion sin implementar en el .ts

--- jpl/src/pages/home/home.ts
+++ jpl/src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
+    persona:usuario =  new usuario();
+    login(){
+      if(this.persona.nombre=="1"){
+        console.log("Iniciando Sesion");
+        alert("bien!");
+      }
+      else{
+        alert("mal");
+      }
+    }
+}
 
+export class usuario{
+  nombre:string;
+  pass:string;
+  constructor(){}
 }
